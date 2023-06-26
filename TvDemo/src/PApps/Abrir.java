807391b7229f2cbcb0b505846ac73063182893a5
Daniel Picon <dannypicon.a04@gmail.com>
2023-06-25 21:34:14 -0500

    Interface Segregation

---
+++ TvDemo/src/PApps/Abrir.java
@@ -0,0 +1,25 @@
+
+package PApps;
+
+/*
+MONTSERRAT ROJAS SANTILLAN
+paquete de la aplicacion de transmision y musica
+*/
+
+/**
+ *
+ * package Acciones
+ */
+public interface Abrir{
+
+    public void abrir();
+
+     
+
+/*
+MONTSERRAT ROJAS SANTILLAN
+finalmente llamamos los metodos para abrir y cerrar las aplicaciones, en dado caso que se desee eliminar la app se llama el
+metodo delete, en dado caso que no se pueda se debe realizar una nueva ejecucion
+*/     
+    
+}
